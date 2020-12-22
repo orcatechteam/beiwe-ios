f4afac61fe7e7c5d6ad81d774ea29e4f720bd73d
Jonathan Lee <jona@ohsu.edu>
2020-12-21 20:58:18 -0800

    Linting

--- Beiwe/Controllers/RegisterViewController.swift
+++ Beiwe/Controllers/RegisterViewController.swift
@@ -11,61 +11,62 @@ import Eureka
 import SwiftValidator
 import PKHUD
 import PromiseKit
+import ObjectMapper
 
 class RegisterViewController: FormViewController {
 
     static let commErrDelay = 7.0
     static let commErr = "The server you are trying to register with is currently unavailable"
-    let autoValidation = false;
-    let db = Recline.shared;
-    var dismiss: ((_ didRegister: Bool) -> Void)?;
+    let autoValidation = false
+    let db = Recline.shared
+    var dismiss: ((_ didRegister: Bool) -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
 
-        let font = UIFont.systemFont(ofSize: 13.0);
-        SVTextRow.defaultCellSetup = { cell, row in
+        let font = UIFont.systemFont(ofSize: 13.0)
+        SVTextRow.defaultCellSetup = { cell, _ in
             cell.textLabel?.font = font
-            cell.detailTextLabel?.font = font;
+            cell.detailTextLabel?.font = font
         }
-        SVPasswordRow.defaultCellSetup = { cell, row in
+        SVPasswordRow.defaultCellSetup = { cell, _ in
             cell.textLabel?.font = font
-            cell.detailTextLabel?.font = font;
+            cell.detailTextLabel?.font = font
         }
         var section = Section("Register for Study")
         section = section <<< SVAccountRow("patientId") {
                 $0.title = "User ID:"
-                $0.placeholder = "User ID";
-                $0.rules = [RequiredRule()]
+                $0.placeholder = "User ID"
+                $0.rules = [RequiredRule(), FloatRule()]
                 $0.autoValidation = autoValidation
+                $0.value = "4"
             }
             <<< SVPasswordRow("tempPassword") {
                 $0.title = "Temporary Password:"
-                $0.placeholder = "Temp Password";
+                $0.placeholder = "Temp Password"
                 $0.rules = [RequiredRule()]
                 $0.autoValidation = autoValidation
             }
             <<< SVPasswordRow("password") {
                 $0.title = "New Password:"
-                $0.placeholder = "New password";
+                $0.placeholder = "New password"
                 $0.rules = [RequiredRule(), RegexRule(regex: Constants.passwordRequirementRegex, message: Constants.passwordRequirementDescription)]
                 $0.autoValidation = autoValidation
             }
             <<< SVPasswordRow("confirmPassword") {
                 $0.title = "Confirm Password:"
-                $0.placeholder = "Confirm Password";
+                $0.placeholder = "Confirm Password"
                 $0.rules = [RequiredRule(), MinLengthRule(length: 1)]
                 $0.autoValidation = autoValidation
             }
-            <<< ButtonRow() {
+            <<< ButtonRow {
                 $0.title = "Register"
                 }
-                .onCellSelection {
-                    [unowned self] cell, row in
-                    if (!self.form.validateAll()) {
-                        print("Bad validation.");
+                .onCellSelection { [unowned self] _, _ in
+                    if !self.form.validateAll() {
+                        print("Bad validation.")
                         return
                     }
                     
@@ -76,26 +77,31 @@ class RegisterViewController: FormViewController {
                     let patientId: String? = formValues["patientId"] as! String?;
                     //let phoneNumber: String? = formValues["phone"] as! String?;
                     let phoneNumber: String? = "NOT_SUPPLIED"
-                    let newPassword: String? = formValues["password"] as! String?;
-                    let tempPassword: String? = formValues["tempPassword"] as! String?;
+                    let newPassword: String? = formValues["password"] as! String?
+                    let tempPassword: String? = formValues["tempPassword"] as! String?
                     if let patientId = patientId, let phoneNumber = phoneNumber, let newPassword = newPassword {
-                        self._registerStudy(patientId: patientId, phoneNumber: phoneNumber, tempPassword: tempPassword, newPassword: newPassword)
+                        self._registerStudy(
+                                patientId: patientId,
+                                phoneNumber: phoneNumber,
+                                tempPassword: tempPassword,
+                                newPassword: newPassword
+                        )
                     }
                 }
-            <<< ButtonRow() {
-                $0.title = "Cancel";
-                }.onCellSelection { [unowned self] cell, row in
+            <<< ButtonRow {
+                $0.title = "Cancel"
+                }.onCellSelection { [unowned self] _, _ in
                     if let dismiss = self.dismiss {
-                        dismiss(false);
+                        dismiss(false)
                     } else {
-                        self.presentingViewController?.dismiss(animated: true, completion: nil);
+                        self.presentingViewController?.dismiss(animated: true, completion: nil)
                     }
         }
 
         form +++ section
 
-        let passwordRow: SVPasswordRow? = form.rowBy(tag: "password");
-        let confirmRow: SVPasswordRow? = form.rowBy(tag: "confirmPassword");
+        let passwordRow: SVPasswordRow? = form.rowBy(tag: "password")
+        let confirmRow: SVPasswordRow? = form.rowBy(tag: "confirmPassword")
         confirmRow!.rules = [ConfirmationRule(confirmField: passwordRow!.cell.textField)]
     }
 
@@ -104,7 +110,7 @@ class RegisterViewController: FormViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func _createStudy(phoneNumber: String, patientId: String, studySettings: StudySettings) -> Promise<Study>{
+    func _createStudy(phoneNumber: String, patientId: String, studySettings: StudySettings) -> Promise<Study> {
         let study = Study(patientPhone: phoneNumber, patientId: patientId, studySettings: studySettings, apiUrl: Constants.apiUrl);
         study.clinicianPhoneNumber = ""
         study.raPhoneNumber = ""
@@ -114,12 +120,12 @@ class RegisterViewController: FormViewController {
         }
         return self.db.save(study)
     }
-    
-    func _registerStudy(patientId: String, phoneNumber: String, tempPassword: String?, newPassword: String ) -> Void {
-        let newBase64Password = Crypto.sharedInstance.sha256Base64URL(newPassword);
-        let registerStudyRequest = RegisterStudyRequest(patientId: patientId, phoneNumber: phoneNumber, newPassword: newBase64Password);
-        ApiManager.sharedInstance.password = tempPassword ?? "";
-        ApiManager.sharedInstance.patientId = patientId;
+
+    func _registerStudy(patientId: String, phoneNumber: String, tempPassword: String?, newPassword: String ) {
+        let newBase64Password = Crypto.sharedInstance.sha256Base64URL(newPassword)
+        let registerStudyRequest = RegisterStudyRequest(patientId: patientId, phoneNumber: phoneNumber, newPassword: newBase64Password)
+        ApiManager.sharedInstance.password = tempPassword ?? ""
+        ApiManager.sharedInstance.patientId = patientId
         ApiManager.sharedInstance.makePostRequest(registerStudyRequest).map { (studySettings, _) in
             PersistentPasswordManager.sharedInstance.storePassword(newPassword)
             return studySettings
@@ -128,47 +134,47 @@ class RegisterViewController: FormViewController {
                 self._createStudy(phoneNumber: phoneNumber, patientId: patientId, studySettings: studySettings)
             }
         }.map { _ in
-            HUD.flash(.success, delay: 1);
+            HUD.flash(.success, delay: 1)
         }.then { _ -> Promise<Bool> in
-            StudyManager.sharedInstance.loadDefaultStudy();
+            StudyManager.sharedInstance.loadDefaultStudy()
         }.done { _ in
             self._dismiss()
         }.catch { error -> Void in
-            print("error received from register: \(error)");
-            var delay = 2.0;
-            var err: HUDContentType;
+            print("error received from register: \(error)")
+            var delay = 2.0
+            var err: HUDContentType
             switch error {
             case ApiErrors.failedStatus(let code):
                 switch code {
                 case 403, 401:
-                    err = .labeledError(title: "Registration failed", subtitle: "Incorrect patient ID or Password");
+                    err = .labeledError(title: "Registration failed", subtitle: "Incorrect patient ID or Password")
                 case 405:
-                    err = .label("UserID already registered on another device.  Please contact your study administrator to unregister any previous devices that may have been used");
+                    err = .label("UserID already registered on another device.  Please contact your study administrator to unregister any previous devices that may have been used")
                     delay = 10.0;
                 case 400:
-                    err = .label("This device could not be registered under the provided patient ID.  Please contact your study administrator");
-                    delay = 10.0;
+                    err = .label("This device could not be registered under the provided patient ID.  Please contact your study administrator")
+                    delay = 10.0
                 default:
-                    err = .label(RegisterViewController.commErr);
+                    err = .label(RegisterViewController.commErr)
                     delay = RegisterViewController.commErrDelay
                 }
             default:
-                err = .label(RegisterViewController.commErr);
+                err = .label(RegisterViewController.commErr)
                 delay = RegisterViewController.commErrDelay
             }
             HUD.flash(err, delay: delay)
         }
     }
-    
-    func _dismiss() -> Void {
-        AppDelegate.sharedInstance().isLoggedIn = true;
+
+    func _dismiss() {
+        AppDelegate.sharedInstance().isLoggedIn = true
         if let dismiss = self.dismiss {
-            dismiss(true);
+            dismiss(true)
         } else {
-            self.presentingViewController?.dismiss(animated: true, completion: nil);
+            self.presentingViewController?.dismiss(animated: true, completion: nil)
         }
     }
-    
+
     /*
      Generates a random offset between -1 and 1 (thats not between -0.2 and 0.2)
     */
@@ -179,13 +185,13 @@ class RegisterViewController: FormViewController {
         }
         return ran
     }
-    
+
     /*
      Generates a random offset between -180 and 180 (thats not between -10 and 10)
     */
     func _generateLongitudeOffset() -> Double {
         var ran = Double.random(in: -180...180)
-        while(ran <= 10 && ran >= -10) {
+        while ran <= 10 && ran >= -10 {
             ran = Double.random(in: -180...180)
         }
         return ran
